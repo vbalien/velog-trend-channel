e5d5204e03c6616f0921f7a766d66f1de28b066f
Jisu Kim <webmaster@alien.moe>
2021-05-12 16:51:27 +0900

    Add path to dockerignore

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,19 @@ if (BOT_TOKEN === undefined) throw new Error("BOT_TOKEN must be provided!");
 const CHANNEL = Deno.env.get("CHANNEL");
 if (CHANNEL === undefined) throw new Error("CHANNEL must be provided!");
 
+// Init mongodb
 const MONGO_HOST = Deno.env.get("MONGO_HOST") || "localhost";
 const MONGO_PORT = parseInt(Deno.env.get("MONGO_PORT") || "27017");
 const mongo = new MongoClient();
 await mongo.connect(`mongo://${MONGO_HOST}:${MONGO_PORT}`);
 
+// Init telegram
+const telegram = new TelegramBot(BOT_TOKEN);
+
+// Init DI
 const serviceCollection = new ServiceCollection();
 serviceCollection.addStatic(types.mongo, mongo);
-serviceCollection.addTransientDynamic(
-  types.telegram,
-  () => new TelegramBot(BOT_TOKEN)
-);
+serviceCollection.addStatic(types.telegram, telegram);
 serviceCollection.addStatic(types.channel, CHANNEL);
 serviceCollection.addTransient<IRepository>(types.IRepository, Repository);
 serviceCollection.addSingleton<IApp>(types.IApp, App);
